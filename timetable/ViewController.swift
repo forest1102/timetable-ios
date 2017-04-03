f5ba30fd43756398b023c9a20334eb6ff73db1f0
issei morita <isseimorita1@gmail.com>
2017-04-03 23:27:37 +0900

    Initial Commit

---
+++ timetable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timetable
+//
+//  Created by 森田一世 on 2017/04/03.
+//  Copyright © 2017年 森田一世. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
